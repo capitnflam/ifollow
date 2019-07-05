28d319a9f3133f83d9b90ee016c864e67e055103
capitN.flam <capitnflam@gmail.com>
2019-07-05 23:38:46 +0200

    Updates

---
+++ precache-manifest.5d7b4c66c475889bd8cfc490adf9f10b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba2101a9b9f3d250b0aa606bdc9afea0",
+    "url": "/ifollow/index.html"
+  },
+  {
+    "revision": "ae6a149c99f4c1c7e4a0",
+    "url": "/ifollow/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/ifollow/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ae6a149c99f4c1c7e4a0",
+    "url": "/ifollow/static/js/main.f2f5d6f3.chunk.js"
+  },
+  {
+    "revision": "5928c9bdf23c5bb0b592",
+    "url": "/ifollow/static/js/runtime~main.ee1f7823.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ifollow/static/media/logo.5d5d9eef.svg"
+  }
+]);
