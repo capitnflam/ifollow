67e522c2d9a48d97d1724459b4c5ebb338a2244e
capitN.flam <capitnflam@gmail.com>
2019-07-08 02:56:02 +0200

    add build watch

--- react-ui/src/components/Header.tsx
+++ react-ui/src/components/Header.tsx
@@ -1,17 +1,63 @@
 import React from 'react'
+import { Theme, withStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
-import Container from '@material-ui/core/Container'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import LibraryAdd from '@material-ui/icons/LibraryAdd'
+import MenuIcon from '@material-ui/icons/Menu'
 
-interface HeaderProps {}
+interface HeaderProps {
+  classes: any
+}
 
-export default class Header extends React.PureComponent<HeaderProps, {}> {
+const styles = (theme: Theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  addButton: {
+    marginLeft: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+})
+
+class Header extends React.PureComponent<HeaderProps, {}> {
   render() {
+    const { classes } = this.props
+
     return (
-      <Container>
-        <AppBar>
-          <h1>Foo</h1>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="Menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              iFollow
+            </Typography>
+            <IconButton
+              edge="end"
+              className={classes.addButton}
+              color="inherit"
+              aria-label="Add"
+            >
+              <LibraryAdd />
+            </IconButton>
+          </Toolbar>
         </AppBar>
-      </Container>
+      </div>
     )
   }
 }
+
+export default withStyles(styles)(Header)
