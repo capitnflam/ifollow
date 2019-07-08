9c912f1d6178ce6259179ed3aff171aea88b7cc3
capitN.flam <capitnflam@gmail.com>
2019-07-08 03:10:23 +0200

    fix mongo uri env var

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ import { Config } from './types'
 const numCPUs = cpus().length
 const isDev = process.env.NODE_ENV !== 'production'
 const port = process.env.PORT || 5000
-const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017'
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017'
 
 const config: Config = {
   isDev,
